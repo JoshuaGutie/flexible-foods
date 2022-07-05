3ab3edac18b0f9126cd8f4d4787231f0cf47f8ab
Josh <52718132+JoshuaGutie@users.noreply.github.com>
2022-07-05 15:22:12 -0500

    search functional

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,64 +1,102 @@
 import React, { Component } from "react";
 import TextField from "@mui/material/TextField";
 import "./css/search.css";
-import Fab from '@mui/material/Fab';
-import AddIcon from '@mui/icons-material/Add';
-
-
+import Fab from "@mui/material/Fab";
+import AddIcon from "@mui/icons-material/Add";
+import SearchIcon from "@mui/icons-material/Search";
 
 class Search extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: '',
-            ingredients:[]};
-    
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
-    
-      handleChange(event) {
-        this.setState({value: event.target.value});
-      }
-    
-      handleSubmit(event) {
-          if(this.state.value != ''){
-        this.setState(prevState => ({
-            ingredients: [...prevState.ingredients, this.state.value]
-          }))
-        console.log(this.state.ingredients)
-        this.setState({value: ''})
-        event.preventDefault();
-      }else {
-      alert('Please input a valid ingredient!' );
-    }}
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+      ingredients: [],
+      mealData: (null),
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(_event) {
+    {
+      /* Fethcing API data*/
+    }
+    fetch(
+      `https://api.spoonacular.com/recipes/findByIngredients?apiKey=bd41dee3858049c3960ddbfde3393060&ingredients=${this.state.ingredients}&number=2`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ mealData: data });
+      })
+      .catch(() => {
+        console.log("error");
+      });
+    console.log(this.state.mealData);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    if (this.state.value != "") {
+      this.setState((prevState) => ({
+        ingredients: [...prevState.ingredients, this.state.value],
+      }));
+      console.log(this.state.ingredients);
+      this.setState({ value: "" });
+
+      event.preventDefault();
+    } else {
+      alert("Please input a valid ingredient!");
+    }
+  }
 
   render() {
     return (
       <div className="searchC">
-          <div className="listC">
-        <form onSubmit={this.handleSubmit}>
+        <div className="listC">
+          <form onSubmit={this.handleSubmit}>
             <TextField
-             label="Your Ingredients"
+              label="Your Ingredients"
               type="text"
               value={this.state.value}
               onChange={this.handleChange}
-              style={{marginTop:"20px"}}
+              style={{ marginTop: "20px" }}
             />
-         <Fab
-          aria-label="save"
-          onClick={this.handleSubmit}
-          style={{marginTop:"15px"}}
-        >
-            <AddIcon />
-        </Fab>
-        </form>
-        <ol className="list">
-        {this.state.ingredients.map(item => {
-          return <li className="item">{item}</li>;
-        })}
-      </ol>
+            <Fab
+              aria-label="save"
+              onClick={this.handleSubmit}
+              style={{ marginTop: "15px" }}
+            >
+              <AddIcon />
+            </Fab>
+          </form>
+          <Fab
+            aria-label="save"
+            onClick={this.handleClick}
+            style={{ margin: "auto" }}
+          >
+            <SearchIcon />
+          </Fab>
+          <ol className="list">
+            {this.state.ingredients.map((item) => {
+              return <li className="item">{item}</li>;
+            })}
+          </ol>
         </div>
+        {this.state.mealData != null && (
+          <div className="mealInfo">
+            {this.state.mealData.map(function (item, index) {
+              return (
+                <ul>
+                  <li key={index}>{item.title}</li> <li >Missing ingredient: {item.usedIngredientCount}</li>
+                </ul>
+              );
+            })}
+          </div>
+        )}
       </div>
     );
   }
